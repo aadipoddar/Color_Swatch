9652c912d3cd5d5ca6756dd871bd1bf8394bd182
aadipoddar <83405769+aadipoddar@users.noreply.github.com>
2021-05-21 11:03:06 +0530

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+		{
+			rb.velocity = Vector2.up * jumpForce;
+		}
+	}
+}
